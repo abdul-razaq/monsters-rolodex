fff0492eaa4013ab1087f0d15dc2e096369e03f0
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-14 15:13:31 +0100

    Fetched users data from an api

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
 
-    this.state = [
-      {
-        name: 'Frankenstein'
-      },
-      {
-        name: 'Dracula'
-      },
-      {
-        name: 'Zombie'
-      },
-    ];
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1',
+        },
+        {
+          name: 'Dracula',
+          id: 'asc2',
+        },
+        {
+          name: 'Zombie',
+          id: 'asc3',
+        },
+      ],
+    };
+  }
+
+  async componentDidMount() {
+    const users = await (
+      await fetch('https://jsonplaceholder.typicode.com/users')
+    ).json();
+    this.setState({ monsters: users });
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
