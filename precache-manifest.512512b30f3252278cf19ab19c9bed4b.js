ec3a334a39a244198dc09c16c1eb8d72605b4fd3
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-15 01:06:08 +0100

    Updates

---
+++ precache-manifest.512512b30f3252278cf19ab19c9bed4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a8a35fb685bc11054ce8a33029d6f2a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a09f42e72315094ed9b6",
+    "url": "/monsters-rolodex/static/css/main.2325bf6b.chunk.css"
+  },
+  {
+    "revision": "d4222e2ffccf92dce4c1",
+    "url": "/monsters-rolodex/static/js/2.e59f8f39.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e59f8f39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a09f42e72315094ed9b6",
+    "url": "/monsters-rolodex/static/js/main.c3e5a50e.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
